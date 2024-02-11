d1b102e3f3b58e69ff8e3119799a648189e32dc3
Sameer Choudhary <72244733+choudhary-sameer@users.noreply.github.com>
2024-02-11 19:09:16 +0530

    Add files via upload
    Uploading local files

---
+++ StrategyPattern/StrategyPattern/src/Main.java
@@ -0,0 +1,24 @@
+import strategy.CreditCardStrategy;
+import strategy.PaypalStrategy;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        ShoppingCart cart = new ShoppingCart();
+
+        Item item1 = new Item("1234",10);
+        Item item2 = new Item("5678",40);
+
+        cart.addItem(item1);
+        cart.addItem(item2);
+
+        //pay by paypal
+        cart.pay(new PaypalStrategy("myemail@example.com", "mypwd"));
+
+        //pay by credit card
+        cart.pay(new CreditCardStrategy("Sameer Choudhary", "1234567890123456", "786", "12/15"));
+    }
+}
